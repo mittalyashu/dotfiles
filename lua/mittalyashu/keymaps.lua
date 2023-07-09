fe0ae72ee3e25e31be7270e3a4d81f7b84fb3be4
Yashu Mittal <mittalyashu77@gmail.com>
2023-07-10 00:09:17 +0530

    feat(refactor): regiter a local variable for opt & keymap

--- lua/mittalyashu/keymaps.lua
+++ lua/mittalyashu/keymaps.lua
@@ -1,14 +1,16 @@
 vim.g.mapleader = " "
 
-vim.keymap.set('v', "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', "K", ":m '-2<CR>gv=gv")
+local keymap = vim.keymap
 
-vim.keymap.set('n', 'J', 'mzJ`z')
+keymap.set('v', "J", ":m '>+1<CR>gv=gv")
+keymap.set('v', "K", ":m '-2<CR>gv=gv")
 
-vim.keymap.set('n', '<C-d>', "<C-d>zz")
-vim.keymap.set('n', '<C-u>', "<C-u>zz")
+keymap.set('n', 'J', 'mzJ`z')
 
-vim.keymap.set('x', '<leader>p', '"_dP')
+keymap.set('n', '<C-d>', "<C-d>zz")
+keymap.set('n', '<C-u>', "<C-u>zz")
 
-vim.keymap.set('n', 'y', '"+y')
-vim.keymap.set('v', 'y', '"+y')
+keymap.set('x', '<leader>p', '"_dP')
+
+keymap.set('n', 'y', '"+y')
+keymap.set('v', 'y', '"+y')
--- lua/mittalyashu/set.lua
+++ lua/mittalyashu/set.lua
@@ -1,28 +1,29 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
+local opt = vim.opt
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+opt.number = true
+opt.relativenumber = true
 
-vim.opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+
+opt.smartindent = true
 
 -- Wrap text lines
-vim.opt.wrap = false
+opt.wrap = false
 
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.hlsearch = false
+opt.incsearch = true
 
 -- Show a few lines of context around the cursor. Note that this makes the text scroll if you mouse-click near the start or end of the window.
-vim.opt.scrolloff = 8
+opt.scrolloff = 8
 
 -- Set the delay to save file in milliseconds
-vim.opt.updatetime = 1000
+opt.updatetime = 1000
 
-vim.opt.colorcolumn = "80"
-
+opt.colorcolumn = "80"
