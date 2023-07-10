449506f77ff0cb282cd161386a1dc7062bfe9ae4
Yashu Mittal <mittalyashu77@gmail.com>
2023-07-10 23:06:03 +0530

    feat: add html, css, scss syntax to TreeSitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "typescript", "go", "vue" },
+  ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "typescript", "go", "vue", "html", "css", "scss" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
